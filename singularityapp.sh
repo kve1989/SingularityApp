52666737965c1c28f02cb3676a7dcd29a24930c7
kve1989 <kve1989@ya.ru>
2024-03-18 19:31:38 +0500

    First init

---
+++ singularityapp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Workaround password store issue
+# Possibly https://github.com/electron/electron/issues/32598
+FLAGS='--password-store=basic'
+
+if [[ $XDG_SESSION_TYPE == "wayland" ]] && [ -c /dev/nvidia0 ]
+then
+    FLAGS="$FLAGS --disable-gpu-sandbox"
+fi
+
+env TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID" zypak-wrapper /app/extra/singularityapp/singularityapp $FLAGS "$@"
